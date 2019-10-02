0c00169c8c9f62f56b957d5b36bb69ad8f3c0389
yakubu rufai mahatma <yrufai23@gmail.com>
2019-10-02 14:51:57 +0000

    slight change to structure

--- client/index.js
+++ client/index.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { Route, Router, IndexRoute, hashHistory } from "react-router";
+
+import "./style/style.css";
+import SongList from "./components/SongList";
+import App from "./components/App";
+import SongCreate from "./components/SongCreate";
+import SongDetail from "./components/SongDetail";
+
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+          <Route path="songs/new" component={SongCreate} />
+          <Route path="songs/:id" component={SongDetail} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector("#root"));
